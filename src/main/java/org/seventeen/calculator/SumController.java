cd5fefe84f2593306bd843aa0832dc9491c7263b
psyyf3 <psyyf3@nottingham.ac.uk>
2018-11-12 12:11:53 +0000

    update

--- src/main/java/org/seventeen/calculator/SumController.java
+++ src/main/java/org/seventeen/calculator/SumController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RestController
 public class SumController {
 
-    private static final String template = "The sum of %f and %f is %f.";
+    //private static final String template = "The sum of %f and %f is %f.";
 
     @RequestMapping("/sum")
     @ResponseBody
-    public Sum sum(@RequestParam(value="num1", defaultValue="0w") double num1,
+    public Sum sum(@RequestParam(value="num1", defaultValue="0") double num1,
                    @RequestParam(value="num2", defaultValue="0") double num2) {
 //        double sum = new Sum(num1, num2).getSum();
 
