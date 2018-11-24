dcd12b639e69d6f09049100564701cf789345a1c
Songkeys <zy22132@nottingham.edu.cn>
2018-11-24 07:58:14 +0000

    Changed api url's to satisfy RESTful specification

--- BackendProject/src/main/java/com/group17/EndpointController.java
+++ BackendProject/src/main/java/com/group17/EndpointController.java
@@ -1,5 +1,7 @@
 package com.group17;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +15,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping(value = "/feedback", produces = "application/hal+json")
 public class EndpointController {
     private final String response_template = "Feedback:\n-Rating: %s\n-Text: %s\n";
     private final String full_response_template = "%s -Link: %s\n";
@@ -20,15 +23,12 @@ public class EndpointController {
     @Autowired
     private FeedbackRepository feedbackDb;
 
-    @PostMapping(
-            value = "/feedback/save",
-            headers = "Accept=application/json"
-    )
-    public ResponseEntity<String> receiveFeedback(HttpServletRequest request, @RequestBody FeedbackEntity feedback) {
+    @PostMapping(headers = "Accept=application/json")
+    public ResponseEntity<String> createFeedback(HttpServletRequest request, @RequestBody FeedbackEntity feedback) {
     	Application.getLogger().info("Endpoint called: [Rating:" + feedback.getRating() 
     									+ ", Text:" + feedback.getText() + "]");
 
-        Tuple<String, HttpStatus> responseTuple = checkFeedback(feedback);
+        Tuple<String, HttpStatus> responseTuple = validateFeedback(feedback);
     	
     	if(!responseTuple.getV().equals(HttpStatus.OK))
     	{
@@ -40,7 +40,7 @@ public class EndpointController {
         feedback.setId(uniqueId.toString());
         feedbackDb.save(feedback);
         String linkUrl = String.format(
-                "%s://%s:%d/feedback/get/%s", request.getScheme(), request.getServerName(),
+                "%s://%s:%d/feedback/%s", request.getScheme(), request.getServerName(),
                 request.getServerPort(), uniqueId.toString()
         );
         Tuple<String, HttpStatus> fullOkResponseTuple = new Tuple<String, HttpStatus>(
@@ -50,7 +50,8 @@ public class EndpointController {
         return new ResponseEntity<String>(fullOkResponseTuple.getK(), fullOkResponseTuple.getV());
     }
     
-    protected Tuple<String, HttpStatus> checkFeedback(FeedbackEntity feedback) {
+    protected Tuple<String, HttpStatus> validateFeedback(FeedbackEntity feedback) {
+        // TODO: Need a better way to seperate validation (error-handling) from making results
         if(feedback.getRating() == null) {
         	Application.getLogger().error("Didn't provide rating");
         	return new Tuple<String, HttpStatus>("No rating provided\n", HttpStatus.BAD_REQUEST);
@@ -64,15 +65,13 @@ public class EndpointController {
         }
     }
 
-    @GetMapping(
-            value = "/feedback/get/{id}"
-    )
-    public ResponseEntity<String> sendFeedback(@PathVariable("id") String id) {
-        Tuple<String, HttpStatus> responseTuple = idDbReturn(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getFeedback(@PathVariable("id") String id) {
+        Tuple<String, HttpStatus> responseTuple = getFeedbackFromDB(id);
         return new ResponseEntity<String>(responseTuple.getK(),responseTuple.getV());
     }
 
-    protected Tuple<String, HttpStatus> idDbReturn(String id) {
+    protected Tuple<String, HttpStatus> getFeedbackFromDB(String id) {
         List<FeedbackEntity> dbResponse = feedbackDb.findById(id);
         if(dbResponse.isEmpty()) {
             Application.getLogger().info("There was no feedback with that id");
