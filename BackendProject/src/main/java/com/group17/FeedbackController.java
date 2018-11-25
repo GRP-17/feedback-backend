051812bb8984ef7d5d346976a8da4bfaa75d669e
Songkeys <zy22132@nottingham.edu.cn>
2018-11-25 05:54:54 +0000

    Removed legacy code

--- BackendProject/src/main/java/com/group17/FeedbackController.java
+++ BackendProject/src/main/java/com/group17/FeedbackController.java
@@ -7,25 +7,14 @@ import org.springframework.hateoas.Resources;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
-//import java.util.UUID;
 import java.util.stream.Collectors;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-//import com.group17.util.Tuple;
-
-//import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @RequestMapping(value = "/feedback", produces = "application/hal+json")
 public class FeedbackController {
-//    private final String response_template = "Feedback:\n-Rating: %s\n-Text: %s\n";
-//    private final String full_response_template = "%s -Link: %s\n";
-
-//    @Autowired
     private final FeedbackRepository repository;
 
     private final FeedbackResourceAssembler assembler;
@@ -56,71 +45,15 @@ public class FeedbackController {
                 .created(new URI(resource.getId().expand().getHref()))
                 .body(resource);
     }
-//    public ResponseEntity<String> createFeedback(HttpServletRequest request, @RequestBody Feedback feedback) {
-//    	Application.getLogger().info("Endpoint called: [Rating:" + feedback.getRating()
-//    									+ ", Text:" + feedback.getText() + "]");
-//
-//        Tuple<String, HttpStatus> responseTuple = validateFeedback(feedback);
-//
-//    	if(!responseTuple.getV().equals(HttpStatus.OK))
-//    	{
-//            return new ResponseEntity<String>(responseTuple.getK(), responseTuple.getV());
-//        }
-//
-//        //TODO - Use this in database storage and also log the data storage
-//        UUID uniqueId = UUID.randomUUID();
-//        feedback.setId(uniqueId.toString());
-//        repository.save(feedback);
-//        String linkUrl = String.format(
-//                "%s://%s:%d/feedback/%s", request.getScheme(), request.getServerName(),
-//                request.getServerPort(), uniqueId.toString()
-//        );
-//        Tuple<String, HttpStatus> fullOkResponseTuple = new Tuple<String, HttpStatus>(
-//                String.format(full_response_template, responseTuple.getK(), linkUrl),
-//                responseTuple.getV()
-//        );
-//        return new ResponseEntity<String>(fullOkResponseTuple.getK(), fullOkResponseTuple.getV());
-//    }
-//
-//    protected Tuple<String, HttpStatus> validateFeedback(Feedback feedback) {
-//        // TODO: Need a better way to seperate validation (error-handling) from making results
-//        if(feedback.getRating() == null) {
-//        	Application.getLogger().error("Didn't provide rating");
-//        	return new Tuple<String, HttpStatus>("No rating provided\n", HttpStatus.BAD_REQUEST);
-//        } else if(feedback.getRating() < 1 || feedback.getRating() > 10) {
-//        	Application.getLogger().error("Rating wasn't between 1 and 10");
-//        	return new Tuple<String, HttpStatus>("Rating wasn't between 1 and 10\n", HttpStatus.BAD_REQUEST);
-//        } else {
-//        	String toReturn = String.format(response_template, feedback.getStars(), feedback.getText());
-//        	Application.getLogger().info("Successfully returned " + toReturn);
-//            return new Tuple<String, HttpStatus>(toReturn, HttpStatus.OK);
-//        }
-//    }
 
     @GetMapping("/{id}")
     Resource<Feedback> findOne(@PathVariable String id) {
 
-        Feedback employee = repository.findById(id)
+        Feedback feedback = repository.findById(id)
                 .orElseThrow(() -> new FeedbackNotFoundException(id));
 
-        return assembler.toResource(employee);
+        return assembler.toResource(feedback);
     }
-//    public ResponseEntity<String> getFeedback(@PathVariable("id") String id) {
-//        Tuple<String, HttpStatus> responseTuple = getFeedbackFromDB(id);
-//        return new ResponseEntity<String>(responseTuple.getK(),responseTuple.getV());
-//    }
-//
-//    protected Tuple<String, HttpStatus> getFeedbackFromDB(String id) {
-//        List<Feedback> dbResponse = repository.findById(id);
-//        if(dbResponse.isEmpty()) {
-//            Application.getLogger().info("There was no feedback with that id");
-//            return new Tuple<String, HttpStatus>("No entry with that id\n", HttpStatus.NOT_FOUND);
-//        } else {
-//            Feedback response =  dbResponse.get(0);
-//            Application.getLogger().info("Entry being returned : " + response.toString());
-//            return new Tuple<String, HttpStatus>(response.toString(), HttpStatus.OK);
-//        }
-//    }
 
     @PutMapping("/{id}")
     ResponseEntity<?> update(@RequestBody Feedback newFeedback, @PathVariable String id) throws URISyntaxException {
