e09ff738a051555c60d625c3fc357035d0700ac5
Songkeys <zy22132@nottingham.edu.cn>
2019-01-04 02:48:27 +0000

    Fix import CrossOrigin

--- BackendProject/src/main/java/com/group17/RootController.java
+++ BackendProject/src/main/java/com/group17/RootController.java
@@ -1,6 +1,7 @@
 package com.group17;
 
 import org.springframework.hateoas.ResourceSupport;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
