0b66992d0c111b500c0bcd766e11cb93fe1f584e
Ben O'Rourke <benjorourke1999@gmail.com>
2019-04-16 01:20:57 +0100

    Revert "Merge branch 'feature-enhancetesting' into development"
    This reverts commit 459345fe719ef12a256ab02f7fd8c72062c2d724, reversing
    changes made to 22a83325d9467b516ffa42e3a14f958bab331550.

--- BackendProject/src/test/java/com/group17/DashboardControllerTest.java
+++ BackendProject/src/test/java/com/group17/DashboardControllerTest.java
@@ -1,72 +1,30 @@
 package com.group17;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.jayway.jsonpath.JsonPath;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runners.MethodSorters;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class DashboardControllerTest extends BaseTest {
-    private static final String CREATE_DASHBOARD_NAME = "dashboard's name";
-
-    private static List<String> dashboardsCreated = new ArrayList<String>();
-
-    @Test
-    public void testAFindAllDashboards() throws Exception {
-        getMockMvc()
-                .perform(get("/dashboard"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.dashboard").isArray());
-    }
-
-    @Test
-    public void testBCreateDashboardEndpoint() throws Exception {
-        String result =
-                getMockMvc()
-                        .perform(
-                                post("/dashboard")
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .content(new String("{\"name\":" + CREATE_DASHBOARD_NAME
-                                                +  "\"}")))
-                        .andExpect(status().isCreated())
-                        .andExpect(jsonPath("$.name").value(CREATE_DASHBOARD_NAME))
-                        .andReturn()
-                        .getResponse()
-                        .getContentAsString();
-
-        dashboardsCreated.add(JsonPath.parse(result).read("$.id"));
-    }
-
-    @Test
-    public void testCUpdateDashboardEndpoint() throws Exception{
-        ResultActions result =
-                (ResultActions) getMockMvc()
-                        .perform(
-                                put("/dashboard")
-                                        .contentType(MediaType.APPLICATION_JSON)
-                                        .content(new String("{\"name\":" + CREATE_DASHBOARD_NAME
-                                                +  "\"}")))
-                        .andExpect(status().isOk())
-                        .andExpect(jsonPath("$.name").value(CREATE_DASHBOARD_NAME))
-                        .andReturn()
-                        .getResponse();
-        dashboardsCreated.add(JsonPath.parse(result).read("$.id"));
-    }
-
-    @Test
-    public void testDDeleteDashboardEndpoint() throws Exception {
-        for (String created : dashboardsCreated) {
-            getMockMvc().perform(delete("/dashboard/" + created))
-                    .andExpect(status().isNoContent());
-        }
-    }
+	
+//	@Test
+//	public void testAFeedback() throws Exception {
+//		getMockMvc()
+//			.perform(get("/dashboard"))
+//			.andExpect(status().isOk())
+//			.andExpect(jsonPath("$.feedback").isArray());
+//	}
+//	
+//	@Test
+//	public void testBAverageRating() throws Exception {
+//		getMockMvc()
+//			.perform(get("/dashboard"))
+//			.andExpect(status().isOk())
+//			.andExpect(jsonPath("$.feedback_rating_average").exists());
+//	}
+	
 }
